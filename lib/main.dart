37b55bfd074a57dd6e1a47c253b7b792d3ed6d48
deba33 <deba.dbs.bkt@gmail.com>
2020-09-23 01:06:05 +0530

    Understanding the App Lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -56,10 +56,28 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   final List<Transaction> _userTransactions = [];
 
   bool _showChart = false;
+
+// Understanding the App Lifecycle
+  // @override
+  // void initState() {
+  //   WidgetsBinding.instance.addObserver(this);
+  //   super.initState();
+  // }
+
+  // @override
+  // void didChangeAppLifecycleState(AppLifecycleState state) {
+  //   print(state)
+  // }
+
+  // @override
+  // dispose() {
+  //   WidgetsBinding.instance.removeObserver(this);
+  //   super.dispose();
+  // }
 
   List<Transaction> get _recentTransactions {
     return _userTransactions.where((tx) {
